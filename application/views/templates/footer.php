65fade8909088ecec9aff47f70e78ce7d976efd9
mangaraja jose <mangarajajose12@gmail.comgit config --global user.email mangarajajose12@gmail.com>
2023-07-28 19:25:00 +0700

    update

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -135,7 +135,7 @@
 
 <!-- BEGIN: Footer-->
 <footer class="footer footer-static footer-light navbar-border navbar-shadow d-print-none">
-	<div class="clearfix blue-grey lighten-2 text-sm-center mb-0 px-2"><span class="float-md-left d-block d-md-inline-block">2023 &copy; Copyright <a class="text-bold-800 grey darken-2" href="#" target="_blank"></a> | Repost by <a href="#" title="" target="_blank">PT. TRI JAYA TISUUE</a></span>
+	<div class="clearfix blue-grey lighten-2 text-sm-center mb-0 px-2"><span class="float-md-left d-block d-md-inline-block">2023 &copy; Copyright <a class="text-bold-800 grey darken-2" href="#" target="_blank"></a> | Repost by <a href="#" title="" target="_blank">PT.KB INSUCARE INDONESIA</a></span>
 		<ul class="list-inline float-md-right d-block d-md-inline-blockd-none d-lg-block mb-0">
 		</ul>
 	</div>
